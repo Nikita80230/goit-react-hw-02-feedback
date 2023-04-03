e93fab5416e82c7e71aff0ec2cd28c4f2ac3fe68
Nikita_Beliy <beliynikita1995@gmail.com>
2023-04-03 23:47:34 +0300

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,79 @@
+import { Component } from "react";
 import { Reviews } from "./Reviews/Reviews";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Reviews/>
-    </div>
-  );
+export class App extends Component {
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    positiveFeedback: 0
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    this.setState((prevState) => {
+          return {
+              positiveFeedback: Math.round(prevState.good/prevState.total *100)
+          }
+      })
+  }
+
+  countTotalFeedback = () => {
+    this.setState((prevState) => {
+          return {
+              total: prevState.good + prevState.neutral + prevState.bad,
+          }
+      })
+  }
+
+  handleClickGood = (event) => {
+      // this.countTotalFeedback()
+      this.setState((prevState) => {
+          return {
+            good: prevState.good + 1,
+          }
+      })
+    this.countTotalFeedback()
+    this.countPositiveFeedbackPercentage()
+  }
+
+  handleClickNeutral = (event) => {
+    // this.countTotalFeedback()
+      this.setState((prevState) => {
+          return {
+              neutral: prevState.neutral + 1, 
+          }
+      })
+    this.countTotalFeedback()
+    this.countPositiveFeedbackPercentage()
+  }
+
+  handleClickBad = (event) => {
+    
+      this.setState((prevState) => {
+          return {
+              bad: prevState.bad + 1,
+          }
+      })
+    this.countTotalFeedback()
+    this.countPositiveFeedbackPercentage()
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Reviews countTotalFeedback={this.countTotalFeedback} handleClickGood={this.handleClickGood} handleClickNeutral={this.handleClickNeutral} handleClickBad={this.handleClickBad} countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage} stateValue={this.state} />
+      </div>
+    );
+  }
 };
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,48 +1,46 @@
 import { Component } from "react"
 import css from "./Reviews.module.css"
 
-export class Reviews extends Component {
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }
-    handleClick = (event) => {
-        this.setState((prevState) => {
-            return {
-                good: prevState.good + 1 
-            }
-        })
-    }
+export const Reviews = ({
+    handleClickGood,
+    handleClickNeutral,
+    handleClickBad, stateValue}) => {
 
-    render(){
+    // state = {
+    //     good: 0,
+    //     neutral: 0,
+    //     bad: 0,
+    //     total: 0
+    // }
+
         return (
             <div>
                 <div className='reviewsContainer'>
                     <h2 className="reviewsContainer__title">Please leave feedback</h2>
                     <ul className={css.list}>
                         <li className="reviewsContainer__item">
-                            <button className={css.btn} onClick={this.handleClick}>GOOD</button>
+                            <button className={css.btn} onClick={handleClickGood}>GOOD</button>
                         </li>
                         <li className="reviewsContainer__item">
-                            <button className={css.btn} onClick={this.handleClick}>NEUTRAL</button>
+                            <button className={css.btn} onClick={handleClickNeutral}>NEUTRAL</button>
                         </li>
                         <li className="reviewsContainer__item">
-                            <button className={css.btn} onClick={this.handleClick}>BAD</button>
+                            <button className={css.btn} onClick={handleClickBad}>BAD</button>
                         </li>
                     </ul>
                 </div>
                 <div className="reviews-results__container">
                     <ul className={css.list}>
-                        <li className="reviews-results__item">Good: {this.state.good}</li>
-                        <li className="reviews-results__item">Neutral: {this.state.neutral}</li>
-                        <li className="reviews-results__item">Bad: {this.state.bad}</li>
+                        <li className="reviews-results__item">Good: {stateValue.good}</li>
+                        <li className="reviews-results__item">Neutral: {stateValue.neutral}</li>
+                        <li className="reviews-results__item">Bad: {stateValue.bad}</li>
+                        <li className="reviews-results__item">Total: {stateValue.total}</li>
+                        <li className="reviews-results__item">Positive feedback: {stateValue.positiveFeedback}%</li>
                     </ul>
                 </div>
             </div>
             
         )
-    } 
 }
 
 
